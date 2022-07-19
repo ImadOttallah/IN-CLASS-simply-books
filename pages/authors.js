56c59c9cb9faad49dc9f77597e0c17a36867651f
Imad Ottallah <imadotta@gmail.com>
2022-07-18 21:17:46 -0500

    added author calls

--- pages/authors.js
+++ pages/authors.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import { getAuthors } from '../api/authorData';
 import AuthorCard from '../components/AuthorCard';
+import { useAuth } from '../utils/context/authContext';
 
 export default function Authors() {
+  const [authors, setAuthors] = useState([]);
+  const { user } = useAuth();
+  const getAllTheAuthors = () => {
+    getAuthors(user.uid).then(setAuthors);
+  };
+  useEffect(() => {
+    getAllTheAuthors();
+  }, [user]);
   return (
-    <>
-      <div><AuthorCard /></div>
-    </>
+    <div className="text-center my-4">
+      {authors.map((author) => (
+        <AuthorCard key={author.firebaseKey} authorObj={author} onUpdate={getAllTheAuthors} />
+      ))}
+    </div>
   );
 }
